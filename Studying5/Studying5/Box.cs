5ceefb6f920ba3c904006116ca1445c91c466b3b
Matheus <matheusluis.dev@gmail.com>
2018-11-05 22:11:25 -0200

    +

---
+++ Studying5/Studying5/Box.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studying5 {
+    class Box {
+        private int length, width, height;
+
+        public Box(int length, int width, int height) {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Length {
+            get { return length; }
+            set { length = value; }
+        }
+
+        public int Width {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public int Height{
+            get { return height; }
+            set { height = value; }
+        }
+
+        public int Volume{
+            get { return length * height * width; }
+        }
+
+        public void ShowBox() {
+            Console.WriteLine($"The volume of the box with width {Width}, " +
+                $"height {Height} and length {Length} is equals to {Volume}.");
+        }
+    }
+}
---
+++ Studying5/Studying5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studying5 {
+    class Program {
+        static void Main(string[] args) {
+            Box box = new Box(2,1,4);
+            box.ShowBox();
+
+            Console.Read();
+        }
+    }
+}
